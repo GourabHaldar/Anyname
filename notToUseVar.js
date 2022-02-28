54e5503ebe413280dfc75fb91ad019846496a78c
GourabHaldar <gourabhaldar11@gmail.com>
2022-02-28 14:34:47 +0530

    my first commit

---
+++ notToUseVar.js
@@ -0,0 +1,11 @@
+//use let instead of var
+
+function print(){
+         for(var i=1;i<=5;i++){
+             console.log(i); //inside loop
+         }
+
+         console.log(i); //outside loop
+}
+
+print()
